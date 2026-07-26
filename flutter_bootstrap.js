d1cb8879b5419e9d0a551421185d898aa8c30baa
almnzoma-web <ao.almnzoma@gmail.com>
2026-07-26 13:19:46 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1260418597" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3186968241" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
